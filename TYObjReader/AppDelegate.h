f779d42590d6f9168d7672633dde04326a0ade36
Tony <tony1991417@hotmail.com>
2014-10-05 22:45:11 -0700

    Initial Commit

---
+++ TYObjReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYObjReader
+//
+//  Created by tony on 14-10-5.
+//  Copyright (c) 2014年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYObjReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYObjReader
+//
+//  Created by tony on 14-10-5.
+//  Copyright (c) 2014年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
